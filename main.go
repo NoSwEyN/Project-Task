02ba063d9fdb14bb9d0e1cb0faa9d095a08e7adc
Denis <Mr.lubenetes@mail.ru>
2025-03-24 15:12:56 +0700

    Первый коммит

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var Task = "Denis"
+
+type RequestBody struct {
+	Task string `json:"task"`
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewDecoder(r.Body).Decode(&RequestBody{})
+	if err := json.NewEncoder(w).Encode(Task); err != nil {
+		fmt.Println(err)
+	}
+	Task = RequestBody{}.Task
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello,"+Task)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/Get", HelloHandler).Methods("GET")
+	router.HandleFunc("/Post", PostHandler).Methods("POST")
+	http.ListenAndServe(":8080", router)
+}
